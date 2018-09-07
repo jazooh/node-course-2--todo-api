fa7f094e8c4ebe56973226c4dfb0a4ee1539e30f
James Ngacha <jazooh@gmail.com>
2018-09-07 11:49:59 +0300

    2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 const MONGO_URl ='mongodb://pirates:pirates500@ds149252.mlab.com:49252/node-js-course2-api'
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URL||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGO_URL||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
