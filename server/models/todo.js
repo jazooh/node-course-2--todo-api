7ed41d25639ece02b6ce83cf894a61c6ebaa4a65
James Ngacha <jazooh@gmail.com>
2018-09-11 11:22:24 +0300

    update config to use json file

--- server/models/todo.js
+++ server/models/todo.js
@@ -2,18 +2,22 @@ var mongoose = require('mongoose');
 
 var Todo = mongoose.model('Todo', {
   text: {
-      type: String,
-      required:true,
-      minlength:1,
-      trim:true
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
- type: Boolean,
- default: false
+    type: Boolean,
+    default: false
   },
   completedAt: {
-  type:Number,
-  default:null
+    type: Number,
+    default: null
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
   }
 });
 
